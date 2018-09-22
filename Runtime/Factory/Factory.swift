a1994a036037fb95b5fd223a8f03cb03e7a5ca09
Eminarcissus <eminarcissus@gmail.com>
2018-09-22 13:23:41 +0800

    changed factory to allow it return nil

--- Runtime/Factory/Factory.swift
+++ Runtime/Factory/Factory.swift
@@ -23,7 +23,7 @@
 import Foundation
 
 
-public func createInstance<T>(constructor: ((PropertyInfo) throws -> Any)? = nil) throws -> T {
+public func createInstance<T>(constructor: ((PropertyInfo) throws -> Any?)? = nil) throws -> T {
     if let value = try createInstance(of: T.self, constructor: constructor) as? T {
         return value
     }
@@ -31,7 +31,7 @@ public func createInstance<T>(constructor: ((PropertyInfo) throws -> Any)? = nil
     throw RuntimeError.unableToBuildType(type: T.self)
 }
 
-public func createInstance(of type: Any.Type, constructor: ((PropertyInfo) throws -> Any)? = nil) throws -> Any {
+public func createInstance(of type: Any.Type, constructor: ((PropertyInfo) throws -> Any?)? = nil) throws -> Any {
     
     if let defaultConstructor = type as? DefaultConstructor.Type {
         return defaultConstructor.init()
@@ -40,25 +40,25 @@ public func createInstance(of type: Any.Type, constructor: ((PropertyInfo) throw
     let kind = Kind(type: type)
     
     #if os(iOS) // does not work on macOS or Linux
-        switch kind {
-        case .struct:
-            return try buildStruct(type: type, constructor: constructor)
-        case .class:
-            return try buildClass(type: type)
-        default:
-            throw RuntimeError.unableToBuildType(type: type)
-        }
+    switch kind {
+    case .struct:
+        return try buildStruct(type: type, constructor: constructor)
+    case .class:
+        return try buildClass(type: type)
+    default:
+        throw RuntimeError.unableToBuildType(type: type)
+    }
     #else // class does not work on macOS or Linux
-        switch kind {
-        case .struct:
-            return try buildStruct(type: type, constructor: constructor)
-        default:
-            throw RuntimeError.unableToBuildType(type: type)
-        }
+    switch kind {
+    case .struct:
+        return try buildStruct(type: type, constructor: constructor)
+    default:
+        throw RuntimeError.unableToBuildType(type: type)
+    }
     #endif
 }
 
-func buildStruct(type: Any.Type, constructor: ((PropertyInfo) throws -> Any)? = nil) throws -> Any {
+func buildStruct(type: Any.Type, constructor: ((PropertyInfo) throws -> Any?)? = nil) throws -> Any {
     let info = try typeInfo(of: type)
     let pointer = UnsafeMutableRawPointer.allocate(byteCount: info.size, alignment: info.alignment)
     defer { pointer.deallocate() }
@@ -67,25 +67,25 @@ func buildStruct(type: Any.Type, constructor: ((PropertyInfo) throws -> Any)? = 
 }
 
 #if os(iOS) // does not work on macOS or Linux
-    func buildClass(type: Any.Type) throws -> Any {
-        let info = try typeInfo(of: type)
-        if let type = type as? AnyClass, var value = class_createInstance(type, 0) {
-            try withClassValuePointer(of: &value) { pointer in
-                try setProperties(typeInfo: info, pointer: pointer)
-                let header = pointer.assumingMemoryBound(to: ClassHeader.self)
-                header.pointee.strongRetainCounts = 2
-            }
-            return value
+func buildClass(type: Any.Type) throws -> Any {
+    let info = try typeInfo(of: type)
+    if let type = type as? AnyClass, var value = class_createInstance(type, 0) {
+        try withClassValuePointer(of: &value) { pointer in
+            try setProperties(typeInfo: info, pointer: pointer)
+            let header = pointer.assumingMemoryBound(to: ClassHeader.self)
+            header.pointee.strongRetainCounts = 2
         }
-        throw RuntimeError.unableToBuildType(type: type)
+        return value
     }
+    throw RuntimeError.unableToBuildType(type: type)
+}
 #endif
 
-func setProperties(typeInfo: TypeInfo, pointer: UnsafeMutableRawPointer, constructor: ((PropertyInfo) throws -> Any)? = nil) throws {
+func setProperties(typeInfo: TypeInfo, pointer: UnsafeMutableRawPointer, constructor: ((PropertyInfo) throws -> Any?)? = nil) throws {
     for property in typeInfo.properties {
-        let value = try constructor.map { (resolver) -> Any in
+        let value = try constructor.map { (resolver) -> Any? in
             return try resolver(property)
-        } ?? defaultValue(of: property.type)
+            } ?? defaultValue(of: property.type)
         
         let valuePointer = pointer.advanced(by: property.offset)
         let sets = setters(type: property.type)
@@ -104,3 +104,4 @@ func defaultValue(of type: Any.Type) throws -> Any {
     
     return try createInstance(of: type)
 }
+
